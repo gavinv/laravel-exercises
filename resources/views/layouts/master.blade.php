d74ef9c287f57266969042d842e6ea759e6d692f
Gavin <gavinvaught@gmail.com>
2016-08-24 10:01:46 -0500

    started working on layouts

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,20 +2,29 @@
 <html lang="en">
 <head>
 	 <title>Reddit</title>
-	    <!-- Latest compiled and minified CSS -->
-	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	 <!-- Latest compiled and minified CSS -->
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-	    <!-- Optional theme -->
-	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	 <!-- Optional theme -->
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Master CSS -->
+	<link rel="stylesheet" href="/css/master.css">
 </head>
 <body>
-	    @if (session()->has('message'))
-	       <div class="alert alert-success alert-dismissible" role="alert">
-		           <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-		           {{ session('message') }}
-	       </div>
-	   @endif
-	   @yield('content')
+	@if (session()->has('SUCCESS_MESSAGE'))
+	<div class="alert alert-success alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		{{ session('SUCCESS_MESSAGE') }}
+	</div>
+	@endif
+	@if (session()->has('ERROR_MESSAGE'))
+	<div class="alert alert-success alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		{{ session('ERROR_MESSAGE') }}
+	</div>
+	@endif
+	@yield('content')
 	<!-- Latest compiled and minified JavaScript -->
 	<script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,25 @@
 @extends('layouts.master')
 
 @section('content')
-	<table>
-@foreach ($posts as $post)
-	<div> <h4><a href="{{ action('PostsController@show', $post->id) }}">{{ $post->title }}</a></h4></div>
-	<small>submitted {{ $post->created_at->diffForHumans() }} by {{ $post->author->username }} to <a href="#">/r/{{ $post->subreddit->name }}</a></small>
-	<br>
-	<div>-----------------------------------------</div>
-@endforeach
+<div class="container">
+	@foreach ($posts as $post)
+	<div class="row"> 
+		<div class="col-xs-3 thumb">
+			<img src="" width="70" height="70">
+		</div>
+		<div class="entry col-xs-9">
+			<div class="title">
+				<a href="{{ action('PostsController@show', $post->id) }}">{{ $post->title }}</a>
+			</div>
+			<div class="tagline">
+				submitted {{ $post->created_at->diffForHumans() }} by <a href="#">{{ $post->author->username }}</a> to <a href="#">/r/{{ $post->subreddit->name }}</a>			
+			</div>
+			<ul class="list-inline">
+				<li><a class ="view-comments" href="{{ action('PostsController@show', $post->id) }}">comments</a></li>
+			</ul>
+		</div>
+	</div>
+	@endforeach
 	{!! $posts->render() !!}
-
+</div>
 @stop
